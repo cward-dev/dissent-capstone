371ce5cf1f47e3c787ab92a2c8762691f62c25b4
Milan <milanstoj94@gmail.com>
2021-02-16 09:20:56 -0600

    update ddl

--- sql/dissent-schema-ddl.sql
+++ sql/dissent-schema-ddl.sql
@@ -2,8 +2,8 @@ drop database if exists dissent;
 create database dissent;
 use dissent;
 
-create table user_login (
-	user_login_id VARCHAR(255) PRIMARY KEY,
+CREATE TABLE user_login (
+    user_login_id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL
 );
@@ -16,7 +16,34 @@ CREATE TABLE `user` (
     country VARCHAR(255),
     bio VARCHAR(255),
     user_login_id VARCHAR(255) NOT NULL,
-    constraint fk_user_user_login_id
-		foreign key (user_login_id)
-        references user_login(user_login_id)
+    CONSTRAINT fk_user_user_login_id FOREIGN KEY (user_login_id)
+        REFERENCES user_login (user_login_id)
 );
+
+CREATE TABLE topic (
+    topic_id VARCHAR(255) PRIMARY KEY,
+    topic_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `source` (
+    source_id VARCHAR(255) PRIMARY KEY,
+    source_name VARCHAR(255),
+    website_url VARCHAR(255),
+    `description` VARCHAR(255)
+);
+
+create table article (
+	article_id VARCHAR(255) PRIMARY KEY,
+    title varchar(255) not null,
+    `description`
+	article_url
+	article_image_url
+	date_published
+	date_posted
+
+    
+
+);
+
+
+
