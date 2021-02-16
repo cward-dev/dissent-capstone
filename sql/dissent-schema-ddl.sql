7139b564e4eeb0ce96621a8ff3ae3415fbbf46bf
Milan <milanstoj94@gmail.com>
2021-02-16 10:10:56 -0600

    added NOT NULL where appropriate

--- sql/dissent-schema-ddl.sql
+++ sql/dissent-schema-ddl.sql
@@ -11,12 +11,12 @@ CREATE TABLE user_login (
 
 CREATE TABLE `user` (
     user_id VARCHAR(255) PRIMARY KEY,
+    user_login_id VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     user_role VARCHAR(255) NOT NULL,
     photo_url VARCHAR(255),
     country VARCHAR(255),
     bio VARCHAR(255),
-    user_login_id VARCHAR(255) NOT NULL,
     CONSTRAINT fk_user_user_login_id FOREIGN KEY (user_login_id)
         REFERENCES user_login (user_login_id)
 );
@@ -29,9 +29,9 @@ CREATE TABLE topic (
 
 CREATE TABLE `source` (
     source_id VARCHAR(255) PRIMARY KEY,
-    source_name VARCHAR(255),
-    website_url VARCHAR(255),
-    `description` VARCHAR(255)
+    source_name VARCHAR(255) NOT NULL,
+    website_url VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE article (
@@ -39,11 +39,11 @@ CREATE TABLE article (
     topic_id VARCHAR(255) NOT NULL,
     source_id VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
-    `description` VARCHAR(255),
-    article_url VARCHAR(255),
+    `description` VARCHAR(255) NOT NULL,
+    article_url VARCHAR(255) NOT NULL,
     article_image_url VARCHAR(255),
-    date_published DATETIME,
-    date_posted DATETIME,
+    date_published DATETIME NOT NULL,
+    date_posted DATETIME NOT NULL,
     CONSTRAINT fk_article_topic_id FOREIGN KEY (topic_id)
         REFERENCES topic (topic_id),
     CONSTRAINT fk_article_source_id FOREIGN KEY (source_id)
@@ -64,7 +64,7 @@ CREATE TABLE article_topic (
 # POST TABLES
 CREATE TABLE tag (
     tag_id VARCHAR(255) PRIMARY KEY,
-    `name` VARCHAR(255)
+    `name` VARCHAR(255) NOT NULL
 );
 
 # POST TABLE
@@ -75,7 +75,7 @@ CREATE TABLE post (
     user_id VARCHAR(255) NOT NULL,
     is_dissenting BOOL NOT NULL,
     date_posted DATETIME NOT NULL,
-    content TEXT(40000), # INDUSTRY MAX CHAR SIZE
+    content TEXT(40000) NOT NULL, # INDUSTRY MAX CHAR SIZE
     CONSTRAINT fk_post_parent_post_id FOREIGN KEY (parent_post_id)
 		REFERENCES post (post_id),
     CONSTRAINT fk_post_article_id FOREIGN KEY (article_id)
@@ -86,9 +86,9 @@ CREATE TABLE post (
 
 # POST-TAG BRIDGE TABLE
 CREATE TABLE post_tag (
-    post_id VARCHAR(255),
-    tag_id VARCHAR(255),
-    count INT UNSIGNED, # UNSIGNED => 0 to 4294967295 v.s. -2147483648 to 2147483647
+    post_id VARCHAR(255) NOT NULL,
+    tag_id VARCHAR(255) NOT NULL,
+    count INT UNSIGNED NOT NULL, # UNSIGNED => 0 to 4294967295 v.s. -2147483648 to 2147483647
         CONSTRAINT pk_post_tag PRIMARY KEY (post_id , tag_id),
     CONSTRAINT fk_post_tag_post_id FOREIGN KEY (post_id)
         REFERENCES post (post_id),
