a5a895b328de49dd465c70b3da70c8e081db85c2
Milan <milanstoj94@gmail.com>
2021-02-16 09:52:59 -0600

    init ddl complete. model and png image added

--- sql/dissent-schema-ddl.sql
+++ sql/dissent-schema-ddl.sql
@@ -1,7 +1,8 @@
-drop database if exists dissent;
-create database dissent;
-use dissent;
+DROP DATABASE IF EXISTS dissent;
+CREATE DATABASE dissent;
+USE dissent;
 
+# USER MANAGEMENT TABLES
 CREATE TABLE user_login (
     user_login_id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
@@ -20,6 +21,7 @@ CREATE TABLE `user` (
         REFERENCES user_login (user_login_id)
 );
 
+# ARTICLE TABLES
 CREATE TABLE topic (
     topic_id VARCHAR(255) PRIMARY KEY,
     topic_name VARCHAR(255) NOT NULL
@@ -32,18 +34,64 @@ CREATE TABLE `source` (
     `description` VARCHAR(255)
 );
 
-create table article (
-	article_id VARCHAR(255) PRIMARY KEY,
-    title varchar(255) not null,
-    `description`
-	article_url
-	article_image_url
-	date_published
-	date_posted
-
-    
-
+CREATE TABLE article (
+    article_id VARCHAR(255) PRIMARY KEY,
+    topic_id VARCHAR(255) NOT NULL,
+    source_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255),
+    article_url VARCHAR(255),
+    article_image_url VARCHAR(255),
+    date_published DATETIME,
+    date_posted DATETIME,
+    CONSTRAINT fk_article_topic_id FOREIGN KEY (topic_id)
+        REFERENCES topic (topic_id),
+    CONSTRAINT fk_article_source_id FOREIGN KEY (source_id)
+        REFERENCES `source` (source_id)
 );
 
+# ARTICLE-TOPIC BRIDGE TABLE
+CREATE TABLE article_topic (
+    article_id VARCHAR(255) NOT NULL,
+    topic_id VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_article_topic PRIMARY KEY (article_id , topic_id),
+    CONSTRAINT fk_article_topic_article_id FOREIGN KEY (article_id)
+        REFERENCES article (article_id),
+    CONSTRAINT fk_article_topic_topic_id FOREIGN KEY (topic_id)
+        REFERENCES topic (topic_id)
+);
 
+# POST TABLES
+CREATE TABLE tag (
+    tag_id VARCHAR(255) PRIMARY KEY,
+    `name` VARCHAR(255)
+);
 
+# POST TABLE
+CREATE TABLE post (
+    post_id VARCHAR(255) PRIMARY KEY,
+    parent_post_id VARCHAR(255),
+    article_id VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    is_dissenting BOOL NOT NULL,
+    date_posted DATETIME NOT NULL,
+    content TEXT(40000), # INDUSTRY MAX CHAR SIZE
+    CONSTRAINT fk_post_parent_post_id FOREIGN KEY (parent_post_id)
+		REFERENCES post (post_id),
+    CONSTRAINT fk_post_article_id FOREIGN KEY (article_id)
+        REFERENCES article (article_id),
+    CONSTRAINT fk_post_user_id FOREIGN KEY (user_id)
+        REFERENCES `user` (user_id)
+);
+
+# POST-TAG BRIDGE TABLE
+CREATE TABLE post_tag (
+    post_id VARCHAR(255),
+    tag_id VARCHAR(255),
+    count INT UNSIGNED, # UNSIGNED => 0 to 4294967295 v.s. -2147483648 to 2147483647
+        CONSTRAINT pk_post_tag PRIMARY KEY (post_id , tag_id),
+    CONSTRAINT fk_post_tag_post_id FOREIGN KEY (post_id)
+        REFERENCES post (post_id),
+    CONSTRAINT fk_post_tag_tag_id FOREIGN KEY (tag_id)
+        REFERENCES tag (tag_id)
+);
