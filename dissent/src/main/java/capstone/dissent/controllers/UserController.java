50a213817ff354f0ced5db68a562b6d054faf244
Milan <milanstoj94@gmail.com>
2021-02-19 11:46:41 -0600

    get all REST complete

--- dissent/src/main/java/capstone/dissent/controllers/UserController.java
+++ dissent/src/main/java/capstone/dissent/controllers/UserController.java
@@ -1,11 +1,28 @@
 package capstone.dissent.controllers;
 
+import capstone.dissent.domain.UserService;
+import capstone.dissent.models.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return service.findAll();
+    }
+
 }
--- dissent/src/main/java/capstone/dissent/controllers/UserLoginController.java
+++ dissent/src/main/java/capstone/dissent/controllers/UserLoginController.java
@@ -1,11 +1,27 @@
 package capstone.dissent.controllers;
 
+import capstone.dissent.domain.UserLoginService;
+import capstone.dissent.models.UserLogin;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/user-login")
 public class UserLoginController {
+
+    private final UserLoginService service;
+
+    public UserLoginController(UserLoginService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<UserLogin> findAll() {
+        return service.findAll();
+    }
 }
