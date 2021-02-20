839155251a8067562fb84560af8bbbf917d0c7b3
cward-dev <cward@dev-10.com>
2021-02-20 11:25:53 -0600

    admin banner setup

--- dissent-react/src/components/AdminBar.js
+++ dissent-react/src/components/AdminBar.js
@@ -1,22 +1,20 @@
+import { useState, useEffect, React } from 'react';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import AddReplyPost from "./post-components/AddReplyPost";
 
 function AdminBar () {
   return (
-    <div className="container alert alert-secondary">
-      <ul class="list-group list-group-horizontal">
-        <div>
-          <li class="list-group-item">Add Topic</li>
-          <li class="list-group-item">Edit Topic</li>
-          <li class="list-group-item">Delete Topic</li>
-        </div>
-        <div>
-          <li class="list-group-item ml-4">Add Articles</li>
-          <li class="list-group-item">Delete Article</li>     
-        </div>
-        <div>
-          <li class="list-group-item ml-4">User</li>
-        </div>
-    </ul>
+    <div className="container alert alert-secondary d-flex flex-row justify-content-center mb-4">
+      <div className="align-self-center"><h5 className="pt-1 mr-4">Admin</h5></div>
+      <Link className="btn btn-dark mr-2" to="/">Add Topic</Link>
+      <Link className="btn btn-dark mr-2" to="/">Edit Topic</Link>
+      <Link className="btn btn-dark mr-2" to="/">Delete Topic</Link>
+      <div className="ml-4" />
+      <Link className="btn btn-dark mr-2" to="/">Add Articles</Link>
+      <Link className="btn btn-dark mr-2" to="/">Delete Article</Link>
+      <div className="ml-4" />
+      <Link className="btn btn-dark mr-2" to="/">Edit User</Link>
+      <Link className="btn btn-dark" to="/">Delete User</Link>
     </div>
   );
 }
