4b7ffe4e895cb280880227421cd8fa824012905f
cward-dev <cward@dev-10.com>
2021-02-21 18:29:10 -0600

    missing </div>

--- dissent-react/src/components/feedback-tag-components/FeedbackTagForm.js
+++ dissent-react/src/components/feedback-tag-components/FeedbackTagForm.js
@@ -126,7 +126,6 @@ function FeedbackTagForm({ object, user }) {
   return (
     <div className="container alert alert-dark">
       <div className="row ">
-      
         <Errors errors={errors} />
         <form onSubmit={handleSubmit}>
           <label htmlFor="feedbackTagId"><h3>Feedback</h3></label>
@@ -139,30 +138,24 @@ function FeedbackTagForm({ object, user }) {
           </select>
           <button type="submit">Submit</button>
         </form>
-
         <div>
-
-        <button onClick={toggleTrueFalseSound}>Sound </button>
-        <button onClick={toggleTrueFalseFallacious}>Fallacious</button>
-        <button onClick={toggleTrueFalseBiased}>Biased</button>
-
-
-
-
-        <Errors errors = {errors}/>
-        <div className="col-4">
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="feedbackTagId"><h3>Feedback</h3></label>
-            <select id="feedbackTagId" name="feedbackTagId" 
-            onChange={onChangeHandler} value = {feedbackTagId}>
-              <option value="">--Give Feedback--</option>
-              <option value="1">Sound</option>
-              <option value="2">Fallacious</option>
-              <option value="3">Biased</option>
-            </select>
-            <button type="submit">Submit</button>
-          </form>
-
+          <button onClick={toggleTrueFalseSound}>Sound </button>
+          <button onClick={toggleTrueFalseFallacious}>Fallacious</button>
+          <button onClick={toggleTrueFalseBiased}>Biased</button>
+          <Errors errors = {errors}/>
+          <div className="col-4">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="feedbackTagId"><h3>Feedback</h3></label>
+              <select id="feedbackTagId" name="feedbackTagId" 
+              onChange={onChangeHandler} value = {feedbackTagId}>
+                <option value="">--Give Feedback--</option>
+                <option value="1">Sound</option>
+                <option value="2">Fallacious</option>
+                <option value="3">Biased</option>
+              </select>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
